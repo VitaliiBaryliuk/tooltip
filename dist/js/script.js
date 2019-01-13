7666e61efd8be48b57edb6d8ad44f5b6680d9623
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-01-13 20:47:05 +0200

    Modified by throttle

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,14 +6,35 @@
   tool.classList.add('tooltip');
   toolRect.classList.add('toolip-rect');
 
-  const containerEventListener = (event) => {
+  function throttle(f, delay) {
+    let counter = 0;
+    setInterval(() => {
+      counter = 0;
+    }, delay);
+    let timerId;
+    return function (a) {
+      if (counter === 0) {
+        f.call(null, a);
+        counter += 1;
+      } else {
+        setTimeout(clearTimeout(timerId), 0);
+        timerId = setTimeout(() => {
+          f.call(null, a);
+        }, delay);
+      }
+    };
+  }
+
+  function setMessage(event) {
     tool.style.left = `${event.pageX - 53}px`;
     tool.style.top = `${event.pageY - 70}px`;
     container.appendChild(tool);
     tool.appendChild(tooltext);
     tool.appendChild(toolRect);
     tooltext.innerHTML = `X: ${event.pageX}, Y:${event.pageY}`;
-  };
+  }
 
-  container.addEventListener('mousemove', containerEventListener);
+  const throttledMessage = throttle(setMessage, 500);
+
+  container.addEventListener('mousemove', throttledMessage);
 }());
